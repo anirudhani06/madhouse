cc1e871762385c4a5fa5830c1fa308c6d82a71af
unknown <anirudhani300@gmail.com>
2023-06-23 15:30:49 +0530

    static files added

---
+++ static/js/app.js
@@ -0,0 +1,115 @@
+const signPage = document.getElementById('signin-page');
+const createRoomPage = document.getElementById('create-room-page');
+const settingsPage = document.getElementById('settings-page');
+const chatRoomPage = document.getElementById('room-page');
+const changePwdPage = document.getElementById('change-password-page');
+const newPwdPage = document.getElementById('new-password-page');
+const loginPage = document.getElementById('login-page');
+const registerPage = document.getElementById('register-page');
+
+if (signPage) {
+  const passwordEye = signPage.querySelector('.password_eye');
+  const input = passwordEye.parentElement.querySelector('input');
+  passwordEye.addEventListener('click', () => {
+    if (passwordEye.classList.contains('on')) {
+      input.setAttribute('type', 'text');
+      passwordEye.classList.remove('on');
+    } else {
+      input.setAttribute('type', 'password');
+      passwordEye.classList.add('on');
+    }
+  });
+}
+
+if (createRoomPage) {
+  const previewImage = createRoomPage.querySelector('#image_input');
+
+  previewImage.addEventListener('change', (event) => {
+    if (event.target.files.length > 0) {
+      const src = URL.createObjectURL(event.target.files[0]);
+      createRoomPage.querySelector('.preview_image').src = src;
+    }
+  });
+}
+
+if (settingsPage) {
+  const previewImage = settingsPage.querySelector('#image_input');
+
+  previewImage.addEventListener('change', (event) => {
+    if (event.target.files.length > 0) {
+      const src = URL.createObjectURL(event.target.files[0]);
+      settingsPage.querySelector('.preview_image').src = src;
+    }
+  });
+}
+
+if (chatRoomPage) {
+  const chatRoom = chatRoomPage.querySelector('#room_chat_box');
+  chatRoom.scrollTop = chatRoom.scrollHeight;
+}
+
+if (changePwdPage) {
+  const eyes = changePwdPage.querySelectorAll('.eye');
+
+  for (let i = 0; i < eyes.length; i++) {
+    eyes[i].addEventListener('click', () => {
+      const currentEye = eyes[i];
+      const inputElem = currentEye.parentElement.querySelector('input');
+      if (currentEye.classList.contains('showpwd')) {
+        currentEye.classList.remove('showpwd');
+        inputElem.setAttribute('type', 'password');
+      } else {
+        currentEye.classList.add('showpwd');
+        inputElem.setAttribute('type', 'text');
+      }
+    });
+  }
+}
+if (newPwdPage) {
+  const eyes = newPwdPage.querySelectorAll('.eye');
+
+  for (let i = 0; i < eyes.length; i++) {
+    eyes[i].addEventListener('click', () => {
+      const currentEye = eyes[i];
+      const inputElem = currentEye.parentElement.querySelector('input');
+      if (currentEye.classList.contains('showpwd')) {
+        currentEye.classList.remove('showpwd');
+        inputElem.setAttribute('type', 'password');
+      } else {
+        currentEye.classList.add('showpwd');
+        inputElem.setAttribute('type', 'text');
+      }
+    });
+  }
+}
+if (registerPage) {
+  const eyes = registerPage.querySelectorAll('.eye');
+  for (let i = 0; i < eyes.length; i++) {
+    eyes[i].addEventListener('click', () => {
+      const currentEye = eyes[i];
+      const inputElem = currentEye.parentElement.querySelector('input');
+      if (currentEye.classList.contains('showpwd')) {
+        currentEye.classList.remove('showpwd');
+        inputElem.setAttribute('type', 'password');
+      } else {
+        currentEye.classList.add('showpwd');
+        inputElem.setAttribute('type', 'text');
+      }
+    });
+  }
+}
+if (loginPage) {
+  const eyes = loginPage.querySelector('.eye');
+
+  eyes.addEventListener('click', () => {
+    const currentEye = eyes;
+    const inputElem = currentEye.parentElement.querySelector('input');
+    if (currentEye.classList.contains('showpwd')) {
+      currentEye.classList.remove('showpwd');
+      inputElem.setAttribute('type', 'password');
+    } else {
+      currentEye.classList.add('showpwd');
+      inputElem.setAttribute('type', 'text');
+    }
+  });
+}
