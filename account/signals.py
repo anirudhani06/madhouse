36b1f40cd9b40e52c033fc95ee7745d344d4a097
unknown <anirudhani300@gmail.com>
2023-06-24 15:03:22 +0530

    profile create and update added in signals

---
+++ account/signals.py
@@ -0,0 +1,34 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth import get_user_model
+from .models import Profile
+
+USER = get_user_model()
+
+
+@receiver(post_save, sender=USER)
+def create_profile(sender, instance, created, *args, **kwargs):
+    profile = Profile()
+
+    if created:
+        profile.user = instance
+        profile.username = instance.username
+        profile.user = instance.user
+        profile.email = instance.email
+        profile.save()
+
+
+@receiver(post_save, sender=Profile)
+def update_profile(sender, instance, created, *args, **kwargs):
+    user = USER.objects.filter(username=instance.username).first()
+
+    if user is not None:
+        if not created:
+            user.username = instance.username
+            user.name = instance.name
+            user.email = instance.email
+            user.save()
+        else:
+            pass
+    else:
+        pass
