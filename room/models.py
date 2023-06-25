829b9d9402e53b63436668bed79cc4843dbe492e
unknown <anirudhani300@gmail.com>
2023-06-25 16:28:11 +0530

    add cover image and absolute url of image

--- room/models.py
+++ room/models.py
@@ -6,14 +6,18 @@ from account.models import Profile
 
 class Room(models.Model):
     name = models.CharField(max_length=40, unique=True)
+    coverpic = models.ImageField(upload_to="rooms/", default="default/room.jpg")
     category = models.ForeignKey(
         "Category", on_delete=models.CASCADE, related_name="rooms"
     )
     owner = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="rooms")
-    members = models.ManyToManyField(Profile, related_name="members")
+    members = models.ManyToManyField(Profile, related_name="members", blank=True)
     is_private = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def get_cover_pic(self):
+        return self.coverpic.url
 
     def __str__(self) -> str:
         return self.name
