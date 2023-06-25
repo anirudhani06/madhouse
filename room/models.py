ee02de6fc604b57f1fb009c48c2e149f0ed523b2
unknown <anirudhani300@gmail.com>
2023-06-25 14:44:44 +0530

    category model created

--- room/models.py
+++ room/models.py
@@ -18,3 +18,16 @@ class Room(models.Model):
     class Meta:
         verbose_name = "Room"
         verbose_name_plural = "Rooms"
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
