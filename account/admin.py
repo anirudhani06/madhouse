22f75f127b28ba49f58484735a58d256595a2fdb
unknown <anirudhani300@gmail.com>
2023-06-25 10:26:24 +0530

    Profile and User model added in admin

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from .models import Profile, User
 
 # Register your models here.
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ["username", "name", "email"]
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ["username", "name", "email"]
+
+
+# group unregistered
+admin.site.unregister(Group)
