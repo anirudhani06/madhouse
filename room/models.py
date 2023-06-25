6b7d7f4f6386d619459c1b46d3618aba5685eabf
unknown <anirudhani300@gmail.com>
2023-06-25 14:43:08 +0530

    room model created

--- room/models.py
+++ room/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from account.models import Profile
 
 # Create your models here.
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=40, unique=True)
+    owner = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name="rooms")
+    members = models.ManyToManyField(Profile, related_name="members")
+    is_private = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        verbose_name = "Room"
+        verbose_name_plural = "Rooms"
