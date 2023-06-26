9acb797c21807103a7ace175830486db3923571b
unknown <anirudhani300@gmail.com>
2023-06-26 16:28:31 +0530

    room and category added in admin pannel

--- room/admin.py
+++ room/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, Room
 
 # Register your models here.
+
+
+@admin.register(Room)
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ["owner", "name", "is_private"]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name", "created_at"]
