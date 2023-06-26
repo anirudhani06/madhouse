1cae6e3f8d2cc9de41413e038cad40ef0c5d0be4
unknown <anirudhani300@gmail.com>
2023-06-26 17:37:18 +0530

    added settinsg and search poeple url

--- account/urls.py
+++ account/urls.py
@@ -1,4 +1,11 @@
 from django.urls import path
 from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path("", views.people, name="people"),
+    path("login/", views.user_login, name="login"),
+    path("logout/", views.user_logout, name="logout"),
+    path("register/", views.user_register, name="register"),
+    path("profile/", views.profile, name="profile"),
+    path("settings/", views.settings, name="settings"),
+]
