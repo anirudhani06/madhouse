6d13f2c32b49a23e013e1fc6b8f963196f045be3
unknown <anirudhani300@gmail.com>
2023-06-26 11:44:43 +0530

    profile view and logout added

--- account/views.py
+++ account/views.py
@@ -1,16 +1,29 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 
+@csrf_exempt
 def user_login(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+
     return render(request, "user/login.html")
 
 
 def user_logout(request):
-    pass
+    logout(request)
+    return redirect("login")
 
 
 @csrf_exempt
@@ -27,3 +40,10 @@ def user_register(request):
 
     context = {"form": form}
     return render(request, "user/register.html", context)
+
+
+@login_required(login_url="login")
+def profile(request):
+    user = request.user.profile
+    context = {"user": user}
+    return render(request, "user/profile.html", context)
