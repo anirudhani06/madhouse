03f4e7fd1fe7d407fabc78055c82cde008ecd915
unknown <anirudhani300@gmail.com>
2023-06-26 20:13:32 +0530

    favourite url removed

--- room/urls.py
+++ room/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path("search/rooms/", views.search_rooms, name="search_rooms"),
     path("create_room/", views.create_room, name="create_room"),
     path("update_room/<str:room_name>/", views.update_room, name="update_room"),
-    path("favourite/", views.favourite, name="favourite"),
 ]
