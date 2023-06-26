5cd462d3ae3c06432b0eb89767daa6f378e8243f
unknown <anirudhani300@gmail.com>
2023-06-26 16:52:47 +0530

    created room form

---
+++ room/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Room
+
+
+class RoomForm(forms.ModelForm):
+    coverpic = forms.ImageField(widget=forms.FileInput({"id": "image_input"}))
+    name = forms.CharField(widget=forms.TextInput({"placeholder": "Room Name"}))
+    category = forms.CharField(
+        widget=forms.TextInput({"placeholder": "Category", "list": "categ_list"})
+    )
+    is_private = forms.BooleanField(widget=forms.CheckboxInput(), required=False)
+
+    class Meta:
+        model = Room
+        fields = ("coverpic", "name", "category", "is_private")
