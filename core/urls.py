62559a0150fb995685ac963ada752bc45857a844
unknown <anirudhani300@gmail.com>
2023-06-26 14:08:26 +0530

    added debug toolbar

--- core/urls.py
+++ core/urls.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+import debug_toolbar
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("room.urls")),
     path("user/", include("account.urls")),
+    path("__debug__/", include(debug_toolbar.urls)),
 ]
 
 
