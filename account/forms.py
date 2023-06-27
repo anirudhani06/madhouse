bae9727705a9eea4b27d46a47550cb833929573a
unknown <anirudhani300@gmail.com>
2023-06-27 12:00:42 +0530

    created profile update form

--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import get_user_model
+from .models import Profile
 
 USER = get_user_model()
 
@@ -17,3 +18,19 @@ class RegisterForm(UserCreationForm):
     class Meta:
         model = USER
         fields = ("name", "username", "email", "password1", "password2")
+
+
+class ProfileForm(forms.ModelForm):
+    avatar = forms.ImageField(
+        widget=forms.FileInput({"id": "image_input"}), required=False
+    )
+    name = forms.CharField(widget=forms.TextInput({"placeholder": "Name"}))
+    username = forms.CharField(widget=forms.TextInput({"placeholder": "Username"}))
+    email = forms.EmailField(widget=forms.EmailInput({"placeholder": "Email"}))
+    bio = forms.CharField(
+        widget=forms.Textarea({"placeholder": "Write about yourself"})
+    )
+
+    class Meta:
+        model = Profile
+        fields = ("avatar", "name", "username", "email", "bio")
