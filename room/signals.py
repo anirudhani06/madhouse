cd8e7defa754f4d16972b544d8b6306d82b8e13c
unknown <anirudhani300@gmail.com>
2023-06-28 11:44:31 +0530

    added slug signals and fixed room members

--- room/signals.py
+++ room/signals.py
@@ -1,11 +1,19 @@
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
 from .models import Room
 from account.models import Profile
+from django.template.defaultfilters import slugify
 
 
-# @receiver(pre_save, sender=Room)
-# def room_create(sender, instance, *args, **kwargs):
-#     room = instance
-#     room.members.add(room.owner.id)
-#     room.save()
+@receiver(pre_save, sender=Room)
+def room_create_slug(sender, instance, *args, **kwargs):
+    room = instance
+    room.slug = slugify(room.name)
+
+
+@receiver(post_save, sender=Room)
+def room_create(sender, instance, created, *args, **kwargs):
+    room = instance
+    if created:
+        room.members.add(room.owner)
+        room.save()
