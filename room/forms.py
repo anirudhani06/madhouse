e88d43344e2d8559dd71a0afd7b0ceec2f96c1ed
unknown <anirudhani300@gmail.com>
2023-06-28 11:42:59 +0530

    set coverpic to required false

--- room/forms.py
+++ room/forms.py
@@ -3,7 +3,9 @@ from .models import Room
 
 
 class RoomForm(forms.ModelForm):
-    coverpic = forms.ImageField(widget=forms.FileInput({"id": "image_input"}))
+    coverpic = forms.ImageField(
+        widget=forms.FileInput({"id": "image_input"}), required=False
+    )
     name = forms.CharField(widget=forms.TextInput({"placeholder": "Room Name"}))
     category = forms.CharField(
         widget=forms.TextInput({"placeholder": "Category", "list": "categ_list"})
