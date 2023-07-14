dbd53ae0905a785b68221a4d2d08d9d6ab5358ed
Anirudh <anirudhani300@gmail.com>
2023-07-14 12:50:48 +0530

    added fav model

--- room/admin.py
+++ room/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Room
+from .models import Category, Room, Favourites
 
 # Register your models here.
 
@@ -12,3 +12,8 @@ class RoomAdmin(admin.ModelAdmin):
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ["name", "created_at"]
+
+
+@admin.register(Favourites)
+class FavouriteAdmin(admin.ModelAdmin):
+    list_display = ["room", "user"]
