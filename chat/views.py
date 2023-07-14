d63e8d344ef2f1ed8e2c2e24d105550f16b9f98b
Anirudh <anirudhani300@gmail.com>
2023-07-14 13:04:13 +0530

    added chat func

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
+from account.models import Profile
+
 
 # Create your views here.
+@login_required(login_url="login")
+def chat(request, username):
+    user = Profile.objects.filter(username=username).first()
+    friends = user.friends.all()
+
+    if user is None:
+        return HttpResponse("User not found")
+    context = {"user": user, "friend_list": friends}
+    return render(request, "chat/chat.html", context)
